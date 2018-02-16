1e406e6c3980e14f25148fef4d2a649b0498c659
JWales13 <joewales13@gmail.com>
2018-02-16 08:39:34 -0600

    added swift file

---
+++ hello_world_swift.playground/Contents.swift
@@ -0,0 +1,17 @@
+print("hello world!")
+
+
+let batarangs = 14
+let smokebombs = 10
+
+let message = "Batman's utility belt has \(batarangs) batarangs and \(smokebombs) smokebombs"
+
+print(message)
+
+
+func craig(on day: String, _ expletive: String) -> String{
+    return "It's \(day), you aint got a job and you aint got \(expletive) to do."
+}
+
+
+print(craig(on:"Friday", "$#!+" ))
